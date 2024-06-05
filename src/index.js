b939900262d13cfbff70df81a8af2a7926177dba
sufian06 <sufian4se@gmail.com>
2024-06-05 20:59:10 +0600

    add DB connection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import dotenv from "dotenv";
 import express from "express";
-dotenv.config();
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "/.env",
+});
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8000;
+
+connectDB();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("⚙️ BatBall app server running ✌️");
 });
 
 app.listen(port, () => {
